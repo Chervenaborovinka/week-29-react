811431ac2d5b110382a23840805bfcea5462a1b0
chervenaborovinka <yogurtmaniac@gmail.com>
2024-04-21 13:55:39 +0300

    created WordContext with api fetch

--- src/components/WordContext/WordContext.jsx
+++ src/components/WordContext/WordContext.jsx
@@ -1,17 +1,13 @@
-import React, { useState, useEffect, createContext } from 'react';
-import WordCard from '../WordCard/WordCard';
-import Table from "../Table/Table";
-import ErrorComponent from "../ErrorComponent/ErrorComponent";
+import { useState, useEffect, } from 'react';
 
-const Context = createContext();
-
-const WordContext = ({ children }) => {
+const WordContext = (url) => {
     const [words, setWords] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://itgirlschool.justmakeit.ru/api/words')
+        const fetchData = () => {
+            fetch(url)
             .then(response => {
                 if (response.ok) {
                     return response.json();
@@ -24,67 +20,14 @@ const WordContext = ({ children }) => {
                 setLoading(false)
             })
             .catch(error => setError(error));
-    }, []);
+        };
 
-    const addWord = (newWord) => {
-        fetch('http://itgirlschool.justmakeit.ru/api/words', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newWord),
-        })
-        .then(response => response.json())
-        .then(data => {
-            setWords([...words, data]);
-        })
-        .catch(error => setError(error));
-    };
+    fetchData();
+        }, [url]);
 
-    const updateWord = (updatedWord) => {
-        fetch('http://itgirlschool.justmakeit.ru/api/words', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(updatedWord),
-    })
-        .then(response => response.json())
-        .then(data => {
-            const updatedWords = words.map((word) => word.id === data.id ? data : word);
-            setWords(updatedWords);
-        })
-        .catch(error => setError(error));
-    };
-
-    const deleteWord = (wordId) => {
-        fetch('http://itgirlschool.justmakeit.ru/api/words', {
-            method: 'DELETE',
-        })
-        .then(() => {
-            const updatedWords = words.filter((word) => word.id !== wordId);
-            setWords(updatedWords);
-        })
-        .catch(error => setError(error));
+        return {words, loading, error};
     };
     
+    export default WordContext;
 
-    return (
-        <Context.Provider value={{ words, addWord, updateWord, deleteWord }}>
-            {loading ? (<p>Loading...</p>
-            ) : error ? (
-                <ErrorComponent message={error} />
-            ) : (
-                <>
-                    <Table />
-                    <WordCard />
-                    {children}
-                </>
-            )}
-        </Context.Provider>
-    );
-
-};
-
-
-export default WordContext;
+    
