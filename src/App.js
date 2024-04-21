da46f6d427f7a47442357fdb6933087d1f69f59c
chervenaborovinka <yogurtmaniac@gmail.com>
2024-04-21 15:50:01 +0300

    constructed fetched words as a table, looked doubled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
 import WordCarousel from './components/WordCarousel/WordCarousel';
-import Table from "./components/Table/Table";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header';
 import ErrorComponent from './components/ErrorComponent/ErrorComponent';
 import WordList from './components/WordList/WordList';
+
 
 function App() {
   return (
--- src/components/WordList/WordList.jsx
+++ src/components/WordList/WordList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import WordContext from '../WordContext/WordContext';
+import styles from './WordList.module.css';
 
 const WordList = () => {
     const {
@@ -9,18 +10,18 @@ const WordList = () => {
     } = WordContext('http://itgirlschool.justmakeit.ru/api/words');
     
     return(
-        <div>
+        <div className={styles.table}>
             {loading ? (<p>Loading...</p>
             ) : error ? (<p>
 
             {error.message}</p>
             ) : (
-                <ul>
+                <ul className={styles.wordcolumn}>
                 {words.map((word) => (
-                    <li>
-                    <span>{word.english}</span>
-                    <span>{word.transcription}</span>
-                    <span>{word.russian}</span>
+                    <li className={styles.wordrow}>
+                    <div >{word.english}</div>
+                    <div>{word.transcription}</div>
+                    <div>{word.russian}</div>
                     </li>
                 ))}
                 </ul>
